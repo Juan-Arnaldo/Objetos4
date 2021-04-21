9b704ec7dbc2364d479c5844079b8dc59fb88757
Juan-Arnaldo <juanarnaldo.00@gmail.com>
2021-04-21 17:36:08 -0300

    creacion de metodos 1

---
+++ src/com/company/VideoStore/VideoStore.java
@@ -0,0 +1,44 @@
+package com.company.VideoStore;
+
+import com.company.Cliente.Cliente;
+import com.company.Pelicula.Pelicula;
+import static com.company.Main.crear;
+
+import java.util.ArrayList;
+
+public class VideoStore {
+    private Pelicula pelicula[];
+    private Cliente cliente[];
+
+    public VideoStore(Pelicula pelicula[], Cliente cliente[]){
+        this.pelicula = pelicula;
+        this.cliente = cliente;
+    }
+
+    public void agregarPelicula(Pelicula aGuardar, ArrayList <Pelicula> listaPelicula){
+        listaPelicula.add(aGuardar);
+    }
+
+    public void agregarCliente(Cliente aGuardar, ArrayList <Cliente> listaCliente){
+        listaCliente.add(aGuardar);
+    }
+
+    public void solicitarPelicula(ArrayList<Pelicula> listaPeliculas, ArrayList<Cliente> listaClientes, String pelicula, String cliente){
+        if(corroborarPelicula(listaPeliculas, pelicula)){
+            if (corroborarStock(listaPeliculas, pelicula)){
+                if (corroborarCliente(listaClientes, cliente)){
+                    System.out.println("Generando Boleta");
+                }else {
+                    crear();
+                }
+            }else{
+                System.out.println("No hay stock de la pelicula ingresada, lo sentimos\n");
+            }
+        }else{
+            System.out.println("La pelicula ingresada no esta disponible\n");
+        }
+    }
+
+
+
+}
