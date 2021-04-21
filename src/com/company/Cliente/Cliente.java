94fbfff33d035eca3e36284294e16410b43953e8
changote <gomez.agustin1896@gmail.com>
2021-04-21 16:12:20 -0300

    Declaracion de clase Pelicula y Cliente

---
+++ src/com/company/Cliente/Cliente.java
@@ -0,0 +1,10 @@
+package com.company.Cliente;
+
+public class Cliente {
+    private String nombre;
+    private double telefono;
+    private String direccion;
+
+
+}
+
---
+++ src/com/company/Pelicula/Pelicula.java
@@ -0,0 +1,15 @@
+package com.company.Pelicula;
+
+public class Pelicula {
+
+    private String genero;
+    private String titulo;
+    private String fechaLanzamiento;
+    private double duracion;
+    private String clasificacionAudiencia;
+    private String paisOrigen;
+    private String descripcion;
+    private int stock;
+
+
+}
