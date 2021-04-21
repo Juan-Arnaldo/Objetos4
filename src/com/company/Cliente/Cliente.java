b81b430787f3dd43e84ee32bfb7fdcbd365e0cfb
changote <gomez.agustin1896@gmail.com>
2021-04-21 18:21:46 -0300

    getters clases

--- src/com/company/Cliente/Cliente.java
+++ src/com/company/Cliente/Cliente.java
@@ -5,6 +5,30 @@ public class Cliente {
     private double telefono;
     private String direccion;
 
+    public Cliente(String nombre, double telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", telefono=" + telefono +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
 }
 
--- src/com/company/Pelicula/Pelicula.java
+++ src/com/company/Pelicula/Pelicula.java
@@ -11,5 +11,59 @@ public class Pelicula {
     private String descripcion;
     private int stock;
 
+    public Pelicula(String genero, String titulo, String fechaLanzamiento, double duracion, String clasificacionAudiencia, String paisOrigen, String descripcion, int stock) {
+        this.genero = genero;
+        this.titulo = titulo;
+        this.fechaLanzamiento = fechaLanzamiento;
+        this.duracion = duracion;
+        this.clasificacionAudiencia = clasificacionAudiencia;
+        this.paisOrigen = paisOrigen;
+        this.descripcion = descripcion;
+        this.stock = stock;
+    }
 
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public String getClasificacionAudiencia() {
+        return clasificacionAudiencia;
+    }
+
+    public String getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String toString() {
+        return "Pelicula{" +
+                "genero='" + genero + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", fechaLanzamiento='" + fechaLanzamiento + '\'' +
+                ", duracion=" + duracion +
+                ", clasificacionAudiencia='" + clasificacionAudiencia + '\'' +
+                ", paisOrigen='" + paisOrigen + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", stock=" + stock +
+                '}';
+    }
 }
