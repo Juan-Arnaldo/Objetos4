215a5114795665d94c4eaea91574814fafb92743
Juan-Arnaldo <juanarnaldo.00@gmail.com>
2021-04-21 16:16:41 -0300

    creacion de clase VideoStore y rama develop

---
+++ src/com/company/VideoStore/VideoStore.java
@@ -0,0 +1,14 @@
+package com.company.VideoStore;
+
+import com.company.Cliente.Cliente;
+import com.company.Pelicula.Pelicula;
+
+public class VideoStore {
+    private Pelicula pelicula[];
+    private Cliente cliente[];
+
+    public void VideoStore(Pelicula pelicula[], Cliente cliente[]){
+        this.cliente = cliente;
+        this.pelicula = pelicula;
+    }
+}
